7215517278da974d85ca57526c2fdd68da990201
GomezFrannco <frangomez.baires@gmail.com>
2023-07-13 10:18:56 -0300

    Ejercicio 2 completo

--- ejercicios/01_entrada_salida/ejercicio_02/app_ES_02.py
+++ ejercicios/01_entrada_salida/ejercicio_02/app_ES_02.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Franco
+apellido: Gomez
 ---
 Ejercicio: entrada_salida_02
 ---
@@ -28,8 +28,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        
-        pass
+        username = prompt(title="User info", prompt="Type your name" )
+        alert(title="Your name is", message=username)
         
         
     
